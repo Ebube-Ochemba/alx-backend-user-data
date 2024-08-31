782e9d04681f750964b95d6421196d1d425cc20f
Ebube-Ochemba <ebube17@gmail.com>
2024-08-31 19:40:19 +0100

    task 2 complete

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """A module for log handling"""
+import logging
 from typing import List
 import re
 
@@ -20,6 +21,25 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = f"({'|'.join(fields)})=.*?({separator}|$)"
     return re.sub(
         pattern,
-        lambda m: f"{m.group(1)}={redaction}{m.group(2)}",
+        lambda match: f"{match.group(1)}={redaction}{match.group(2)}",
         message
         )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+    
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+    
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the log record with redacted fields."""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
+    
