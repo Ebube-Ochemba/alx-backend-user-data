3c0741e1f66421b7d38fba8bae471abb6e8bb06d
Ebube-Ochemba <ebube17@gmail.com>
2024-09-06 20:18:59 +0100

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,3 +47,24 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value.
+
+        Returns:
+            A tuple containing the user email and password,
+            or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string into user email and password
+        user_credentials = decoded_base64_authorization_header.split(":", 1)
+        return user_credentials[0], user_credentials[1]
