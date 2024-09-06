1dc1bd3c3336aab5b5624dc3c736d66f45aac5f8
Ebube-Ochemba <ebube17@gmail.com>
2024-09-06 21:21:22 +0100

    revert

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,9 +14,7 @@ class Auth:
         - Returns True if the path is not in excluded_paths.
         - Path and excluded_paths must be slash tolerant.
         """
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         # Ensure both the path and excluded_paths end with '/'
@@ -24,15 +22,10 @@ class Auth:
             path += '/'
 
         for excluded_path in excluded_paths:
-            if excluded.endswith('*'):
-                # Check if the path starts with the portion before the '*'
-                if path.startswith(excluded[:-1]):
-                    return False
-            else:
-                if excluded[-1] != '/':
-                    excluded += '/'
-                if path == excluded:
-                    return False
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
 
         return True
 
