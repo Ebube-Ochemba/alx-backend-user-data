1d98bd31d6c192f20f03434867b6245a5b8ab437
Ebube-Ochemba <ebube17@gmail.com>
2024-09-07 19:50:45 +0100

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Authentication module for the API.
 """
+import os
 from flask import request
 from typing import List, TypeVar
 
@@ -45,3 +46,14 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """ Retrieves the current user from the request. """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns the value of the session cookie from the request """
+        if request is None:
+            return None
+
+        # Get the session cookie name from the environment variable
+        session_name = os.getenv("SESSION_NAME")
+
+        # Use .get() to safely retrieve the cookie value
+        return request.cookies.get(session_name)
