01ec07bbe5560e35e1e0f79996efe2b6a1413f08
Ebube-Ochemba <ebube17@gmail.com>
2024-09-07 15:36:15 +0100

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session Authentication module for the API.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session authentication class that inherits from Auth"""
+    pass
