331f024e390fc93105c29931438607ae837beca3
Ebube-Ochemba <ebube17@gmail.com>
2024-09-07 16:40:51 +0100

    pep8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ from typing import TypeVar
 
 class SessionAuth(Auth):
     """Session authentication class that inherits from Auth"""
-    
+
     # Class attribute to store session IDs and corresponding user IDs
     user_id_by_session_id = {}
 
@@ -17,11 +17,11 @@ class SessionAuth(Auth):
         """Create a session ID for a given user ID and store it in memory"""
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         # Generate a unique session ID
         session_id = str(uuid.uuid4())
-        
+
         # Store the session ID and corresponding user ID in the dictionary
         self.user_id_by_session_id[session_id] = user_id
-        
+
         return session_id
