a21177890751e9a1458b41e930eb9c9b24822c85
Ebube-Ochemba <ebube17@gmail.com>
2024-09-12 00:30:56 +0100

    remove logging

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -30,21 +30,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
-        Args:
-            email (str): The user's email
-            hashed_password (str): The user's hashed password
-        Returns:
-            User: The User object that was added
-        """
-        # Create a new User instance
+        """Add a user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
-
-        # Add the new user to the session
         self._session.add(new_user)
-
-        # Commit the session to save the user to the database
         self._session.commit()
-
-        # Return the new User object
         return new_user
