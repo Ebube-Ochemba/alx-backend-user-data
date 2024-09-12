987c482cb6ae4a357f292353bedd4c31917e3a70
Ebube-Ochemba <ebube17@gmail.com>
2024-09-13 00:20:01 +0100

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,3 +92,18 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the password of a user based on the reset_token."""
+        user = self._db.find_user_by(reset_token=reset_token)
+
+        if not user:
+            raise ValueError("Invalid reset token")
+
+        # Hash the new password using bcrypt
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),
+                                        bcrypt.gensalt())
+
+        # Update user's password and invalidate the reset token
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
