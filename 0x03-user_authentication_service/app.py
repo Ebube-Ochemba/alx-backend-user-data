c3ffbd26f4ed88c4d647d7d22a0add84e8e38371
Ebube-Ochemba <ebube17@gmail.com>
2024-09-13 00:13:19 +0100

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,13 +47,11 @@ def logout():
     """DELETE /sessions route for logging out."""
     # Retrieve session_id from the cookie
     session_id = request.cookies.get("session_id")
-
     if session_id is None:
         return abort(403)
 
     # Find the user with the session_id
     user = AUTH.get_user_from_session_id(session_id)
-
     if user is None:
         return abort(403)
 
@@ -66,17 +64,31 @@ def logout():
 def profile():
     """GET /profile route to get user profile using session_id cookie."""
     session_id = request.cookies.get("session_id")
-
     if session_id is None:
         return abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
-
     if user is None:
         return abort(403)
 
     # Return the user's email in the response
     return jsonify({"email": user.email}), 200
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """
+    POST /reset_password route for generating a password reset token.
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(400, description="Email is required")
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
